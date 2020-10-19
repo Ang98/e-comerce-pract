de332a24c611602e5f12aa1257b166f67aee76b9
Ang98 <angelcayatopa98@gmail.com>
2020-10-19 11:20:51 -0500

    antes gitignore

---
+++ accounts/models/__init__.py
@@ -0,0 +1,3 @@
+from .client import Client
+from .employee import Employee
+from .password_reset import PasswordReset
--- accounts/models/client.py
+++ accounts/models/client.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Client(models.Model):
     
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone_number = models.charField(max_length=9)
+    phone_number = models.CharField(max_length=9)
     created = models.DateField(auto_now_add=True)
     modified = models.DateField(auto_now=True)
     
